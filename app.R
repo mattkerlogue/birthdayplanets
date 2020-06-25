fad0ecb677c36726840918ad008e3f74133210e5
mattkerlogue <matt.kerlogue@me.com>
2020-06-25 15:49:13 +0100

    Use fixed_grid, add next birthday

--- app.R
+++ app.R
@@ -12,11 +12,10 @@ library(tidyverse)
 
 planet_data <- readRDS("data/planet_data.RDS")
 
-my_planet_age_plot <- function(birthday, planet_data) {
-  
-  my_birthday <- birthday
-  
-  lifetime <- my_birthday %>%
+
+calc_my_planet_age <- function(birthday, planet_data) {
+
+  lifetime <- birthday %>%
     lubridate::interval(Sys.Date()) %>% 
     lubridate::int_length()
   
@@ -25,16 +24,26 @@ my_planet_age_plot <- function(birthday, planet_data) {
   my_planet_age <- planet_data %>%
     mutate(
       planetary_age = round(lifetime_hours / orbit_in_hours, 1),
+      next_birthday_in = round((((trunc(planetary_age) + 1) * orbit_in_hours) - 
+        lifetime_hours)/24),
       label_position = case_when(
         planet == "Jupiter" ~ 1.25,
         planet == "Saturn" ~ 1.25,
         planet == "Uranus" ~ 1.15,
         planet == "Neptune" ~ 1.15,
         TRUE ~ 1.1
-      )
-    ) %>%
-    select(planet, au, radius, planetary_age, label_position) %>%
+      )) %>%
+    select(planet, au, radius, orbit_in_hours, planetary_age, next_birthday_in, label_position) %>%
     mutate(label = paste(planetary_age, planet, "years"))
+  
+  
+  return(my_planet_age)
+    
+}
+
+my_planet_age_plot <- function(birthday, planet_data) {
+  
+  my_planet_age <- calc_my_planet_age(birthday, planet_data)
   
   planet_colours <- c(
     "Mercury" = "seashell4",
@@ -67,31 +76,81 @@ my_planet_age_plot <- function(birthday, planet_data) {
   
 }
 
+next_planet_birthday <- function(birthday, planet_data, what) {
+  
+  my_planet_age <- calc_my_planet_age(birthday, planet_data)
+  
+  next_birthday <- my_planet_age %>%
+    filter(next_birthday_in == min(next_birthday_in))
+  
+  if (what == "where") {
+    next_birthday_out <- next_birthday$planet
+  } else if (what == "when") {
+    next_birthday_out <- format(Sys.Date() + lubridate::days(next_birthday$next_birthday_in),
+                                format = "%a %d %b %Y")
+  } else if (what == "age") {
+    next_birthday_out <- trunc(next_birthday$planetary_age) + 1
+  }
+  
+  return(next_birthday_out)
+  
+}
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Birthday Planets",
-                 tabPanel(emo::ji("alien"),
-                          sidebarLayout(
-                            sidebarPanel(
-                              "Hello Human! I cake in peace!", HTML("<br /><br />"),
-                              "The planets in your solar system orbit the sun at different rates, use this tool to find out how old you are on different planets in your solar system!",
-                              HTML("<br /><br />"),
-                              dateInput("birthday",
-                                        "When is your birthday?",
-                                        "2000-01-01"),
-                              HTML("<br />This tool will represent the time since you were born into a years on each planet of the solar system, plus Pluto (which isn't a planet but we like to think it is). It does this by first converting the time since your birthday into hours, this figure is then divided by the orbital period of each planet in hours and rounded to one decimal place<br />"),
-                              HTML("<br />The sizes of the planets and their relative distance from the sun are based on <a href='https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html'>data from NASA</a>, but have been scaled to render in the plot.")
-                            ),
-                            mainPanel(plotOutput("planet_plot", width = "400px", height = "800px"))
-                          )))
+ui <- navbarPage("Birthday Planets", tabPanel(
+    emo::ji("alien"),
+    fixedRow(column(
+      12,
+      fixedRow(column(
+        4,
+        fixedRow(column(
+          12,
+          HTML("Hello Human! I cake in peace!<br /><br />",
+               "The planets in your solar system orbit the sun at different ",
+               "rates, use this tool to find out how old you are on different ",
+               "planets in your solar system!<br /><br />"),
+          dateInput("birthday", "When is your birthday?", "2000-01-01"),
+          HTML("Your next interplanetary birthday will be:<h2>"),
+          textOutput("next_birthday_when", inline = TRUE),
+          HTML("</h2> When you will be <strong>"),
+          textOutput("next_birthday_age", inline = TRUE),
+          HTML("</strong> years old on <strong>"),
+          textOutput("next_birthday_where", inline = TRUE),
+          HTML("</strong>")))),
+        column(
+          8,plotOutput("planet_plot", height = "600px"))))),
+    fixedRow(column(
+      12,
+      HTML("<br />This tool represents the time since you were born ",
+           "as years on each planet of the solar system, plus Pluto ",
+           "(which isn't a planet but some of you like to think it is). ",
+           "It does this by first converting the time since your birthday ",
+           "into hours, this figure is then divided by the orbital period ",
+           "of each planet in hours and rounded to one decimal place<br />",
+           "<br />The sizes of the planets and their relative distance ",
+           "from the sun are based on ",
+           "<a href=\"https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html\">",
+           "data from NASA</a>, but have been scaled to render in the plot.")))),
+    fluid = FALSE)
 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
   output$planet_plot <- renderPlot(
         my_planet_age_plot(input$birthday, planet_data)
     )
+  
+  output$next_birthday_where <- renderText(
+    next_planet_birthday(input$birthday, planet_data, "where")
+  )
+  
+  output$next_birthday_when <- renderText(
+    next_planet_birthday(input$birthday, planet_data, "when")
+  )
+  
+  output$next_birthday_age <- renderText(
+    next_planet_birthday(input$birthday, planet_data, "age")
+  )
 }
 
 # Run the application 
